6d14a6e7e9dccca0babaa85e2bd5d84931baf932
prazeresmath <matheus.prazeres@hotmail.com>
2024-08-16 05:24:49 -0300

    Criando site_setup e melhorando parte administrativa do django

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blog",
+    "site_setup",
 ]
 
 MIDDLEWARE = [
@@ -63,6 +64,8 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "site_setup.context_processors.context_processor_example",
+                "site_setup.context_processors.site_setup",
             ],
         },
     },
